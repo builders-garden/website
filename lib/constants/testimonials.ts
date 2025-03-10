fdea2c5893ad77d544f6bf7eafde6be07a6a2d22
lim0n3 <simonestaffa96@gmail.com>
2025-03-10 18:12:29 +0000

    fix formats

--- lib/constants/testimonials.ts
+++ lib/constants/testimonials.ts
@@ -4,7 +4,7 @@ export const TESTIMONIALS = [
     about: "Co-founder, Ponder",
     description:
       "“Builders Garden helped us bring a brand new V2 frame to life in just a few weeks. All I did was provide an outline and they worked with me to finalize the vision. Super responsive team as well. A+”",
-    image: "/testimonials/cojo-pfp.png",
+    image: "/testimonials/cojo-pfp.jpg",
     brandImage: "/testimonials/ponder-logo.gif",
   },
   {
@@ -12,7 +12,7 @@ export const TESTIMONIALS = [
     about: "Co-founder, Talent Protocol",
     description:
       "“I've been working with Builders Garden for a few months now and they've been a great partner. They've helped us bring a brand new V2 frame to life in just a few weeks. All I did was provide an outline and they worked with me to finalize the vision. Super responsive team as well. A+”",
-    image: "/testimonials/macedo-pfp.png",
+    image: "/testimonials/macedo-pfp.jpg",
     brandImage: "/testimonials/talent-protocol.png",
   },
   {
@@ -20,7 +20,7 @@ export const TESTIMONIALS = [
     about: "Staff, Ephemera Labs",
     description:
       "“I've been working with Builders Garden for a few months now and they've been a great partner. They've helped us bring a brand new V2 frame to life in just a few weeks. All I did was provide an outline and they worked with me to finalize the vision. Super responsive team as well. A+”",
-    image: "/testimonials/humanagent-pfp.png",
+    image: "/testimonials/humanagent-pfp.jpg",
     brandImage: "/testimonials/ephemera-logo.png",
   },
 ];

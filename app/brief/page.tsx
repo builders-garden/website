30ad2fe4c0058eddceadf976063ff7f06c71ad59
lim0n3 <simonestaffa96@gmail.com>
2025-05-13 12:07:29 +0100

    brief

--- app/brief/page.tsx
+++ app/brief/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-export default function BrandKit() {
+export default function Brief() {
   redirect("https://www.canva.com/design/DAGnT0CPbTs/fmt1ypvfimBZlhmY8TLV2g/view");
 }
 

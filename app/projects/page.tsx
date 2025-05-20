1b8dd52a6c26585d7273e772633fe17fb8fb4aee
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2025-05-20 10:43:23 +0200

    fix max w

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,7 @@ import Projects from "@/components/projects";
 export default function ProjectsHome() {
   return (
     <section className="">
-      <div className="max-w-[1440px] w-full mx-auto">
+      <div className="w-full mx-auto">
         <div className="w-full px-5">
           <BackButton link="/" />
         </div>

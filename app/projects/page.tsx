730363626211ffe3bc5d942efe99d20605586c29
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2025-05-29 18:48:36 +0200

    fix padding

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,8 +4,8 @@ import Projects from "@/components/projects";
 export default function ProjectsHome() {
   return (
     <section className="">
-      <div className="max-w-[1450px] w-full mx-auto px-5 2xl:px-0">
-        <div className="w-full">
+      <div className="max-w-[1450px] w-full mx-auto">
+        <div className="w-full px-5 2xl:px-0">
           <BackButton link="/" />
         </div>
         <Projects showViewAll={false} />

e2658f3d553a2ed7680be08fbce82ed80a9ab39e
lim0n3 <simonestaffa96@gmail.com>
2025-06-05 19:28:37 +0200

    fix copy

--- lib/constants/services.ts
+++ lib/constants/services.ts
@@ -1,30 +1,33 @@
 export const SERVICES = [
   {
     name: "Onchain Consumer Apps",
-    description: "End-to-end development of consumer-first decentralized applications and smart contracts.",
+    description:
+      "End-to-end development of consumer-first decentralized applications and smart contracts.",
     icon: "/services/mini-apps.svg",
     color: "primary",
-    link: "#dapp-development"
+    link: "#dapp-development",
   },
   {
     icon: "/services/farcaster.svg",
     name: "Farcaster Development",
     description:
-      "Leverage Farcaster’s decentralized social ecosystem to create powerful, onchain experiences like frames.",
+      "Leverage Farcaster’s decentralized social ecosystem to create powerful, onchain experiences like miniapps.",
     color: "secondary",
     link: "https://devpost.com/software/split3",
   },
   {
     icon: "/services/mvp.svg",
     name: "R&D and Prototyping",
-    description: "Rapid prototyping and research services to validate your ideas and quickly bring solutions to life.",
+    description:
+      "Rapid prototyping and research services to validate your ideas and quickly bring solutions to life.",
     color: "secondary",
     link: "https://ethglobal.com/showcase/ghost-mae3q",
   },
   {
     icon: "/services/ai-agents.svg",
     name: "Developer Experience Consulting",
-    description: "Comprehensive review and feedback on your SDK, protocol implementation, and documentation to improve developer experience.",
+    description:
+      "Comprehensive review and feedback on your SDK, protocol implementation, and documentation to improve developer experience.",
     color: "primary",
     link: "https://devfolio.co/projects/papabase-7d6b",
   },
